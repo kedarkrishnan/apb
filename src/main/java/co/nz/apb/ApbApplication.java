54386678aa781a0575129a19131b0bbb42806c61
Kedar Krishnan <Kedar@Macintosh-2.local>
2017-05-01 19:43:06 +1200

    Initial Commit

---
+++ src/main/java/co/nz/apb/ApbApplication.java
@@ -0,0 +1,12 @@
+package co.nz.apb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApbApplication.class, args);
+	}
+}
