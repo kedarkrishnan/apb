bc458fbc4bef17912e4007174c106450a4875fb2
Kedar Krishnan <kedar.krishnan@gmail.com>
2017-05-08 09:25:47 +1200

    Removed app load test

--- src/test/java/co/nz/apb/ApbApplicationTests.java
+++ src/test/java/co/nz/apb/ApbApplicationTests.java
@@ -1,11 +1,5 @@
 package co.nz.apb;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class ApbApplicationTests {
 
 	public void contextLoads() {
