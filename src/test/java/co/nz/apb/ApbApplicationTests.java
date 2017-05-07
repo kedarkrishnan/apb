9233a2b75ea4a05e08c7cbf41357bad8ed41e8c5
Kedar Krishnan <kedar.krishnan@gmail.com>
2017-05-08 09:24:29 +1200

    Suppress app load test

--- src/test/java/co/nz/apb/ApbApplicationTests.java
+++ src/test/java/co/nz/apb/ApbApplicationTests.java
@@ -1,6 +1,5 @@
 package co.nz.apb;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,7 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApbApplicationTests {
 
-	@Test
 	public void contextLoads() {
 	}
 
